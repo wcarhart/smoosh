ab7417f17fc56f336152156fcb6d284611be9636
Will Carhart <wcarhart@sandiego.edu>
2018-06-09 13:32:32 -0700

    Finished sentence parsing

--- smoosh.py
+++ smoosh.py
@@ -3,9 +3,12 @@
 import sys
 import re
 
+ABB = ['etc', 'mr', 'mrs', 'ms', 'dr', 'sr', 'jr', 'gen', 'rep', 'sen', 'st', 'al', 'eg', 'ie', 'in', 'phd', 'md', 'ba', 'dds', 'ma', 'mba', 'us', 'usa']
+
 def extract_words(filename):
   f = open(filename, 'rU')
-  text = f.read()
+  text_ = f.read()
+  text = text_.replace('\n', ' ')
   f.close()
 
   words = text.split()
@@ -14,19 +17,62 @@ def extract_words(filename):
   sentences = []
   str_buffer = ''
 
-  for ch in text:
-    if ch == '.' or ch == '?' or ch == '!':
-      balh
+  # EOS = end of sentence
+  ## checking for EOS
+  ## if EOS --> append to buffer ('.'), add buffer to array, clear buffer
+  ## if !EOS --> append to buffer
+  for index, ch in enumerate(text):
+    if ch == '?' or ch == '!':
+      if text[index+1] == ' ':
+        # EOS
+        str_buffer += ch
+        sentences.append(str_buffer)
+        str_buffer = ''
+      else:
+        # !EOS
+        str_buffer += ch
+    elif ch == '.':
+      if text[index+1] == ' ':
+        if isAbbreviation(str_buffer):
+          # !EOS
+          str_buffer += ch
+        elif text[index+2].upper() == text[index+2]:
+          # EOS
+          str_buffer += ch
+          sentences.append(str_buffer)
+          str_buffer = ''
+      else:
+        # !EOS
+        str_buffer += ch
     else:
-      
+      # !EOS
+      str_buffer += ch
 
-
-  sentences = text.split('.')
-  print sentences
+  for index, sentence in enumerate(sentences):
+    print str(index) + ": " + sentence
 
   #print sorted(count_words(cleaned_words).items(), key=custom_sort)
 
   return text
+
+def isAbbreviation(text):
+  search = text[::-1]
+  buff = ''
+
+  for ch in search:
+    if not ch == ' ':
+      buff += ch
+    else:
+      break
+
+  buff = buff[::-1]
+  buff = buff.lower()
+  buff = buff.replace('.', '')
+
+  if buff in ABB:
+    return True
+  else:
+    return False
 
 def custom_sort(s):
   return s[-1]
