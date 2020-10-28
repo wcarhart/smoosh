c21869faaa0dd5c783739c6c5553881d8b9966dc
Will Carhart <carhartwill@gmail.com>
2020-10-28 14:51:11 -0700

    Added timeout for web requests, updated comments and improved filtering

--- smoosh.py
+++ smoosh.py
@@ -3,16 +3,23 @@ import bs4
 import os
 import re
 import requests
+import signal
 import sys
 import unidecode
+
+# determine if text is processed or not
+PROCESSED_TEXT = False
 
 # we can't split sentences on abbreviations that end in a '.'
 ABB = [
     'etc', 'mr', 'mrs', 'ms', 'dr', 'sr',
     'jr', 'gen', 'rep', 'sen', 'st', 'al',
     'eg', 'ie', 'in', 'phd', 'md', 'ba',
-    'dds', 'ma', 'mba', 'us', 'usa', 'inc',
-    'pm', 'am'
+    'dds', 'ma', 'mba', 'inc', 'pm', 'am',
+    'jan', 'feb', 'mar', 'apr', 'jun', 'jul',
+    'aug', 'sep', 'sept', 'oct', 'nov', 'dec',
+    'mon', 'tue', 'wed', 'weds', 'thur',
+    'thu', 'thurs', 'fri', 'fig'
 ]
 
 # if we come across a word with a '.' that ends in one of these common file
@@ -28,6 +35,7 @@ EXT = [
     'bz2', 'csv', 'cs', 'erl', 'asm', 'awk', 'bat', 'bmp',
     'class', 'dll', 'dump', 'exe', 'hpp', 'jar', 'log', 
     'obj', 'rc', 'ts', 'rs', 'wav', 'zip', 'com', 'nl',
+    'ms'
 ]
 
 # we should exclude these common words when scoring sentences to get more
@@ -42,7 +50,8 @@ EXCLUDE = [
     'these', 'you', 'this', 'they', 'we', 'our',
     'will', 'are', 'am', 'can', 'an', 'have', 'how',
     'my', 'which', 'their', 'theirs', 'what', 'her',
-    'him', 'had', 'would', 'them'
+    'him', 'had', 'would', 'them', 'like', 'than',
+    'could', 'did', 'do'
 ]
 
 # if we see a lot of these symbols, the sentence is likely code and should
@@ -53,6 +62,23 @@ CODE = [
     '(', ')', '$', '&', '%', '^', '_', '#', '~',
     '`'
 ]
+
+# strip away these tags from the HTML to attempt to pull out meaningful text
+# content in these tags is usually not relevant to text meaning
+TAGS = [
+    'script', 'style', 'h1', 'h2', 'h3', 'h4',
+    'h5', 'h6', 'nav', 'title', 'svg', 'footer'
+]
+
+# if a tag has a class with one of these key words, it is usually not
+# relevant to text meaning
+CLASS_KEYWORDS = [
+    'nav', 'menu', 'copyright'
+]
+
+# custom exception for when timeouts occur
+class TimeoutException(Exception):
+    pass
 
 # try to pull text out of source
 def get_text(source):
@@ -65,42 +91,59 @@ def get_text(source):
             with open(source) as f:
                 text = f.read()
         except:
-            print(f'Could not read from file \'{source}\'')
+            print(f'Could not read from local file \'{source}\'')
             sys.exit(1)
     else:
         try:
+            # get HTML from webpage
             html = requests.get(source)
             status_code = html.status_code
+
+            # convert HTML to meaningful text data with 3 steps
+            #  1. convert HTML to plaintext
+            #  2. remove irrelevant tags
+            #  3. remove navigation links
             soup = bs4.BeautifulSoup(html.content, 'html.parser')
-            for script in soup(['script', 'style']):
+            for script in soup(TAGS):
                 script.extract()
+            for tag in soup.find_all():
+                if 'class' in tag.attrs:
+                    for class_name in CLASS_KEYWORDS:
+                        if class_name in ' '.join(tag.attrs['class']).lower():
+                            tag.extract()
             text = soup.get_text()
+
+            # update timeout settings
+            global PROCESSED_TEXT
+            PROCESSED_TEXT = True
+        except TimeoutException:
+            sys.exit(1)
         except:
-            print(f'Could not get data from URL \'{source}\'')
+            print(f'Could not fetch data from URL')
             sys.exit(1)
 
     if not status_code == 200:
-        print(f'({status_code}) Could not scrape data from URL \'{source}\'')
+        print(f'({status_code}) Could not scrape data, unable to access data programmatically from URL')
         sys.exit(1)
 
     # decode text to ASCII-ish
     try:
         text = unidecode.unidecode(text)
     except:
-        print(f'Could not decode data from \'{source}\'')
+        print(f'Could not decode parsed data')
         sys.exit(1)
 
     # replace persnickity characters
-    text = text.replace('\n', ' ').replace(' " ', ' ').replace(" ' ", '')
+    text = text.replace('\n', ' ').replace(' " ', ' ').replace(" ' ", ' ')
     if text == '' or text == None or not '.' in text:
-        print(f'Could not parse data from \'{source}\'')
+        print(f'Could not parse text, either no sentences found in data or data format could not be processed')
         sys.exit(1)
     return text
 
 # build resolved sentences from a block of text
 def get_sentences(text):
-    sentences = [chunk for chunk in text.split('.') if not chunk == '' and not chunk.isspace()]
-    # sentences = [chunk for chunk in re.split('[.?!]', text) if not chunk == '' and not chunk.isspace()]
+    sentences = [chunk for chunk in re.split('([.?!])', text) if not chunk == '' and not chunk.isspace()]
+    sentences = [x+y for x,y in zip(sentences[0::2], sentences[1::2])]
 
     # we'll need to make sure we split up our sentences properly based on ABB and EXT
     index = 0
@@ -116,7 +159,7 @@ def get_sentences(text):
             not last_word_previous_sentence in ['a', 'i']
             and not first_word_next_sentence in ['a', 'i']
         ):
-            sentences[index] = '.'.join([sentences[index], sentences[index + 1]])
+            sentences[index] = ''.join([sentences[index], sentences[index + 1]])
             del sentences[index + 1]
         else:
             index += 1
@@ -156,7 +199,7 @@ def calculate_sentence_scores(sentences, frequencies):
             score += frequencies[raw_word]
         scores.append(score)
 
-    # weight first 10% of text heavier, as most news articles are front-loaded
+    # weight first 10% of text 50% heavier, as most news articles are front-loaded
     sentence_scores = list(zip(sentences, scores))
     for index in range(int(len(sentence_scores)*0.1)):
         sentence_scores[index] = (sentence_scores[index][0], int(sentence_scores[index][1]*1.5))
@@ -179,8 +222,10 @@ def build_summary(scores, limit):
             summary_sentences.append(scores[sorted_sentences[index][0]][0])
 
     # clean up text and convert to string
-    summary = '. '.join(summary_sentences) + '.'
-    summary = ' '.join(summary.split())
+    # ~94% of English words are less than 14 characters, so we exclude words
+    # longer than 14 characters to attempt to further clarify the text
+    summary = ' '.join(summary_sentences)
+    summary = ' '.join([word for word in summary.split() if len(word) < 14])
     return summary
 
 # build the metrics string based on summary properties
@@ -228,18 +273,39 @@ def build_parser():
     parser.add_argument('-n', '--sentence-limit', type=int, default=7, required=False, help='the number of sentences that will be used to describe the text')
     parser.add_argument('-o', '--omit-metrics', action='store_true', required=False, help='omit metric summary')
     parser.add_argument('-v', '--verbose', action='store_true', required=False, help='print verbose metric summary')
+    parser.add_argument('-t', '--timeout', type=int, default=10, required=False, help='timeout (in seconds) to use when fetching data from a URL')
     return parser
 
+# raise an exception if timeout occurs
+def signal_handler(signum, frame):
+    raise TimeoutException('timeout')
+
 def main():
+    # parse args
     parser = build_parser()
     args = parser.parse_args()
-    text = get_text(args.source)
-    sentences = get_sentences(text)
-    frequencies = calculate_word_frequency(sentences)
-    scores = calculate_sentence_scores(sentences, frequencies)
-    summary = build_summary(scores, args.sentence_limit)
-    metrics = build_metrics(text, summary, frequencies, args.omit_metrics, args.verbose)
-    print_results(summary, metrics)
+
+    # set up timeout
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(args.timeout)
+
+    try:
+        text = get_text(args.source)
+        sentences = get_sentences(text)
+        # for s in sentences:
+        #     print(s)
+        #     print('')
+        # sys.exit()
+        frequencies = calculate_word_frequency(sentences)
+        scores = calculate_sentence_scores(sentences, frequencies)
+        summary = build_summary(scores, args.sentence_limit)
+        metrics = build_metrics(text, summary, frequencies, args.omit_metrics, args.verbose)
+        print_results(summary, metrics)
+    except:
+        if not PROCESSED_TEXT:
+            print(f'Timeout occurred while trying to fetch data from URL')
+            sys.exit(1)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
